37734e0b0f00a8413db59866140c45eb408e27d6
Anonymous <anonymous@example.com>
2024-07-16 00:33:56 +0530

    remove

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 const axiosPublic =  axios.create({
-    baseURL: 'https://complete-foodi-server-vjyn.onrender.com/',
+    baseURL: 'https://complete-foodi-server-vjyn.onrender.com',
   })
 
 const useAxiosPublic = () => {
