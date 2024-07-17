97a9abb1644155cb069d75aa5a028f431b8934a6
Sachin Verma <97831442+sachin27verma@users.noreply.github.com>
2024-07-17 13:55:02 +0530

    Update useAxiosPublic.jsx

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 const axiosPublic =  axios.create({
-    baseURL: ' https://complete-foodi-server-whx8.onrender.com/',
+    baseURL: ' https://complete-foodi-server-whx8.onrender.com',
   })
 
 const useAxiosPublic = () => {
